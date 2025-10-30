651587001fc6eefd0191955e3e75a08b510901ae
Walid Newaz <mdwalid@gmail.com>
2025-10-30 16:26:27 -0600

    feat(chapter) Removed unused code.

--- section_01_getting_started/chapter_01_introduction/fastapi-todo/fastapi_todo/dependencies.py
+++
@@ -1,8 +0,0 @@
-from fastapi import HTTPException, Header
-
-# Route dependency
-def get_api_key(x_api_key: str = Header(...)) -> str:
-    """Simple dependency to check for API key header."""
-    if x_api_key != "secret-key":
-        raise HTTPException(status_code=403, detail="Forbidden: Invalid API key")
-    return x_api_key
