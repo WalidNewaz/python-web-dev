c22422e08278ff83505962a4bbe4a6029f407949
Walid Newaz <mdwalid@gmail.com>
2025-11-27 02:00:16 -0700

    fix(*): Ignore sqlite DBs during commits.

--- chapter_9/blogsite/src/apps/blog/views.py
+++ chapter_9/blogsite/src/apps/blog/views.py
@@ -3,23 +3,22 @@
 # This file contains all the views.
 # ============================================================
 
-from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from .forms import PostForm
+from .models import Post
 
-# Temporary storage for posts
-POSTS = []
 
 def home(request):
-    """Render homepage with all blog posts."""
-    return render(request, "blog/home.html", {"posts": POSTS})
+    """Handle GET (show form) and POST (save form to DB)."""
+    posts = Post.objects.all().order_by("-created_at")
+    return render(request, "blog/home.html", {"posts": posts})
 
 def create_post(request):
     """Handle GET (show form) and POST (process form)."""
     if request.method == "POST":
         form = PostForm(request.POST)
         if form.is_valid():
-            POSTS.append(form.cleaned_data)
+            Post.objects.create(**form.cleaned_data)
             return redirect("home")
     else:
         form = PostForm()
