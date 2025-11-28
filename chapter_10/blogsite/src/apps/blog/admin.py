2fc6227e15be95f0344c3d88d38b0088f6d00747
Walid Newaz <mdwalid@gmail.com>
2025-11-28 01:20:26 -0700

    feat(admin): Registered a Django app model with the admin

--- chapter_10/blogsite/src/apps/blog/admin.py
+++ chapter_10/blogsite/src/apps/blog/admin.py
@@ -1,3 +1,10 @@
+# ============================================================
+# Register App Models with the Admin
+# ============================================================
+
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Post
+
+admin.site.register(Post)
