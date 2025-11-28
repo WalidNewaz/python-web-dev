e443815da988b2aa3ab24489af23e3a1ee7ce6e1
Walid Newaz <mdwalid@gmail.com>
2025-11-28 02:32:49 -0700

    feat(admin): Updated model registration with Django admin

--- chapter_10/blogsite/src/apps/blog/admin.py
+++ chapter_10/blogsite/src/apps/blog/admin.py
@@ -4,7 +4,13 @@
 
 from django.contrib import admin
 
-from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+# admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ("id", "title", "content", "created_at")
+    search_fields = ("title","content")
+    list_filter = ("created_at",)
+
